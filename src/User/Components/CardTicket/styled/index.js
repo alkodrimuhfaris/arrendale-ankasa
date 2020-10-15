5ce0febcaeee03f7fc6230f560930ad0d4fccd47
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-10-16 02:36:19 +0700

    intilize component card ticket

---
+++ src/User/Components/CardTicket/styled/index.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    width: 878px;
+    height: 240px;
+    background: #FFFFFF;
+    border-radius: 15px;
+`;
+
+export const Logo = styled.img`
+    width: 100px;
+    height: 57px;
+`;
+
+export const Airline = styled.span`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px;
+    color: #595959;
+`;
+
+export const TextDestination = styled.h1`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 22px;
+    color: #000000;
+`;
+
+export const AirlineIcon = styled.img``;
+
+export const Period = styled.span`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 22px;
+    text-align: center;
+    color: #595959;
+`;
+
+export const Text = styled.span`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 22px;
+    color: #6B6B6B;
+`;
+
+export const Icon = styled.img``;
+
+export const Price = styled.span`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px;
+
+    /* identical to box height, or 137% */
+    text-align: right;
+
+    color: #2395FF;
+`;
+
+export const SelectButton = styled.button`
+    width: 150px;
+    height: 50px;
+    background: #2395FF;
+    box-shadow: 0px 8px 10px rgba(35, 149, 255, 0.3);
+    border-radius: 10px;
+`;
+
+export const TextButton = styled.span`
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    color: #FFFFFF;
+`;
+
+export const ViewDetails = styled.button`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    color: #2395FF;
+`;
