578d1db7c2d6aec6f2924c31b8ec0991852a102a
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-10-16 20:59:13 +0700

    fix linter warnings🚨

--- src/User/App.jsx
+++ src/User/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 // import Login from "./Pages/Login";
 // import Signup from "./Pages/Signup";
@@ -13,23 +13,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Notofications from "./Pages/Notofications";
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    {/* <Route path="/auth/login" component={Login} exact/> */}
-                    {/* <Route path="/auth/signup" component={Signup} exact/> */}
-                    {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
-                    {/* <Route path="/find" component={Explore} exact/> */}
-                    {/* <Route path="/find/search/result" component={SearchResult} exact/> */}
-                    {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
-                    {/* <Route path="/user/booking" component={MyBooking} exact/> */}
-                    {/* <Route path="/user/booking/detail" component={BookingDetail} exact/> */}
-                    {/* <Route path="/user/profile" component={Profile} exact/> */}
-                    {/* <Route path="/user/Notifications" component={Notifications} exact/> */}
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          {/* <Route path="/auth/login" component={Login} exact/> */}
+          {/* <Route path="/auth/signup" component={Signup} exact/> */}
+          {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
+          {/* <Route path="/find" component={Explore} exact/> */}
+          {/* <Route path="/find/search/result" component={SearchResult} exact/> */}
+          {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
+          {/* <Route path="/user/booking" component={MyBooking} exact/> */}
+          {/* <Route path="/user/booking/detail" component={BookingDetail} exact/> */}
+          {/* <Route path="/user/profile" component={Profile} exact/> */}
+          {/* <Route path="/user/Notifications" component={Notifications} exact/> */}
+        </Switch>
+      </Router>
+    );
+  }
 }
 export default App;
