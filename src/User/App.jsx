37f6885b3bea1b46f8b9c283b2ac33ebd6cf56fd
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-10-19 00:31:00 +0700

    initialize search result page

--- src/User/App.jsx
+++ src/User/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Signup from "./Pages/Signup";
 // import ForgotPassword from "./Pages/ForgotPassword";
 // import Explore from "./Pages/Explore";
-// import SearchResult from "./Pages/SearchResult";
+import SearchResult from "./Pages/SearchResult/";
 // import FlightDetails from "./Pages/FlightDetails";
 // import MyBooking from "./Pages/MyBooking";
 // import BookingDetail from "./Pages/BookingDetail";
@@ -13,23 +13,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Notofications from "./Pages/Notofications";
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    {/* <Route path="/auth/login" component={Login} exact/> */}
-                    {/* <Route path="/auth/signup" component={Signup} exact/> */}
-                    {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
-                    {/* <Route path="/find" component={Explore} exact/> */}
-                    {/* <Route path="/find/search/result" component={SearchResult} exact/> */}
-                    {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
-                    {/* <Route path="/user/booking" component={MyBooking} exact/> */}
-                    {/* <Route path="/user/booking/detail" component={BookingDetail} exact/> */}
-                    {/* <Route path="/user/profile" component={Profile} exact/> */}
-                    {/* <Route path="/user/Notifications" component={Notifications} exact/> */}
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          {/* <Route path="/auth/login" component={Login} exact/> */}
+          {/* <Route path="/auth/signup" component={Signup} exact/> */}
+          {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
+          {/* <Route path="/find" component={Explore} exact/> */}
+          <Route path="/find/search/result" component={SearchResult} exact/>
+          {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
+          {/* <Route path="/user/booking" component={MyBooking} exact/> */}
+          {/* <Route path="/user/booking/detail" component={BookingDetail} exact/> */}
+          {/* <Route path="/user/profile" component={Profile} exact/> */}
+          {/* <Route path="/user/Notifications" component={Notifications} exact/> */}
+        </Switch>
+      </Router>
+    );
+  }
 }
 export default App;
