0a32f92f4f1d0c0031677a21be5709b19b12caf7
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-10-19 00:27:00 +0700

    create components for search result page

--- src/User/Components/CardTicket/index.jsx
+++ src/User/Components/CardTicket/index.jsx
@@ -1,11 +1,134 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import React from "react";
+import {
+  Card,
+  Logo,
+  Airline,
+  TextDestination,
+  AirlineIcon,
+  Period,
+  Text,
+  Price,
+  SelectButton,
+  TextButton,
+  ViewDetails,
+  Icon
+} from "./styled";
+import { Row, Col, Container } from "reactstrap";
 
-export default () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import flightIcon from "../../Assets/flight.svg";
+import luggage from "../../Assets/luggage.svg";
+import meal from "../../Assets/meal.svg";
+import wifi from "../../Assets/wifi.svg";
+import downIcon from "../../Assets/down.svg";
 
+const CardTicket = (props) => {
+  return (
+    <Card className="mt-3">
+      <Container>
+        <Row className="align-items-center">
+          <Col sm={2}>
+            <Logo src={props.logoAirlines} alt="" />
+          </Col>
+          <Col sm={10}>
+            <Airline>{props.nameAirlines}</Airline>
+          </Col>
+        </Row>
+        <Row className="align-items-center mt-4">
+          <Col sm={3}>
+            <Row>
+              <Col sm={4}>
+                <Row>
+                  <Col>
+                    <TextDestination>{props.departure}</TextDestination>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Text>{props.departureTime}</Text>
+                  </Col>
+                </Row>
+              </Col>
+              <Col sm={3}>
+                <AirlineIcon src={flightIcon} alt="" />
+              </Col>
+              <Col sm={4}>
+                <Row>
+                  <Col>
+                    <TextDestination>{props.arrived}</TextDestination>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Text>{props.arrivedTime}</Text>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={3}>
+            <Row className="justify-content-center">
+              <Col>
+                <Period>{props.period}</Period>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Text>
+                  ({props.transit !== 0 ? props.transit : null}&nbsp;
+                  {props.transit !== 0 ? "Transit" : "Direct"})
+                </Text>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm="auto" className="m-0 p-0">
+            <Row>
+              <Col>
+                <Icon
+                  src={props.facilities[0].length ? luggage : ""}
+                  className="m-1"
+                />
+                <Icon
+                  src={props.facilities[1].length ? meal : ""}
+                  className="m-1"
+                />
+                <Icon
+                  src={props.facilities[2].length ? wifi : ""}
+                  className="m-1"
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col sm="auto">
+            <Row>
+              <Col className="align-self-center">
+                <Price>
+                  {props.price}
+                  <Text>/pax</Text>
+                </Price>
+              </Col>
+            </Row>
+          </Col>
+          <Col sm="auto">
+            <SelectButton type="button" onClick={props.onClick}>
+              <TextButton>Select</TextButton>
+            </SelectButton>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="auto">
+            <ViewDetails to={props.viewDetails} onClick={props.onClick}>
+              View Details
+              <Icon src={downIcon} alt="" className="ml-2"/>
+            </ViewDetails>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+};
+
+
+export default CardTicket;
 
--- src/User/Components/CardTicket/styled/index.js
+++ src/User/Components/CardTicket/styled/index.js
@@ -1,86 +1,87 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Card = styled.div`
-    width: 878px;
-    height: 240px;
-    background: #FFFFFF;
-    border-radius: 15px;
+  padding: 28px 0 28px 0;
+  background: #ffffff;
+  border-radius: 15px;
 `;
 
 export const Logo = styled.img`
-    width: 100px;
-    height: 57px;
+  width: 100px;
+  height: 57px;
 `;
 
 export const Airline = styled.span`
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 22px;
-    color: #595959;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
+  color: #595959;
 `;
 
 export const TextDestination = styled.h1`
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 22px;
-    color: #000000;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 22px;
+  color: #000000;
+  vertical-align: middle;
 `;
 
 export const AirlineIcon = styled.img``;
 
-export const Period = styled.span`
-    font-style: normal;
-    font-weight: normal;
-    font-size: 16px;
-    line-height: 22px;
-    text-align: center;
-    color: #595959;
+export const Period = styled.h4`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  color: #595959;
 `;
 
-export const Text = styled.span`
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 22px;
-    color: #6B6B6B;
+export const Text = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 22px;
+  color: #6b6b6b;
+  text-align: center;
 `;
 
 export const Icon = styled.img``;
 
 export const Price = styled.span`
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 22px;
-
-    /* identical to box height, or 137% */
-    text-align: right;
-
-    color: #2395FF;
+  display: flex;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
+  color: #2395ff;
+  vertical-align: middle;
 `;
 
 export const SelectButton = styled.button`
-    width: 150px;
-    height: 50px;
-    background: #2395FF;
-    box-shadow: 0px 8px 10px rgba(35, 149, 255, 0.3);
-    border-radius: 10px;
+  width: 150px;
+  height: 50px;
+  background: #2395ff;
+  box-shadow: 0px 8px 10px rgba(35, 149, 255, 0.3);
+  border-radius: 10px;
+  border: none;
 `;
 
-export const TextButton = styled.span`
-    font-style: normal;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 24px;
-    color: #FFFFFF;
+export const TextButton = styled.h4`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
 `;
 
-export const ViewDetails = styled.button`
-    font-style: normal;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 24px;
-    color: #2395FF;
+export const ViewDetails = styled(Link)`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: #2395ff;
 `;
