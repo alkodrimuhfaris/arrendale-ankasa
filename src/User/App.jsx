4237d4db641c312a0dce98c0dfa67728000cebaa
Muhammad <32612909+Bawazier@users.noreply.github.com>
2020-10-20 14:27:26 +0700

    Update App.jsx

--- src/User/App.jsx
+++ src/User/App.jsx
@@ -4,13 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Login from "./Pages/Login";
 // import Signup from "./Pages/Signup";
 // import ForgotPassword from "./Pages/ForgotPassword";
-<<<<<<< HEAD
 import Explore from "./Pages/Explore";
-// import SearchResult from "./Pages/SearchResult";
-=======
-// import Explore from "./Pages/Explore";
 import SearchResult from "./Pages/SearchResult/";
->>>>>>> origin/development
 // import FlightDetails from "./Pages/FlightDetails";
 import MyBooking from "./Pages/MyBooking";
 // import BookingDetail from "./Pages/BookingDetail";
@@ -18,26 +13,6 @@ import MyBooking from "./Pages/MyBooking";
 // import Notifications from "./Pages/Notifications";
 
 class App extends React.Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    {/* <Route path="/auth/login" component={Login} exact/> */}
-                    {/* <Route path="/auth/signup" component={Signup} exact/> */}
-                    {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
-                    <Route path="/find" component={Explore} exact/>
-                    {/* <Route path="/find/search/result" component={SearchResult} exact/> */}
-                    {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
-                    {/* <Route path="/user/booking" component={MyBooking} exact/> */}
-                    {/* <Route path="/user/booking/detail" component={BookingDetail} exact/> */}
-                    {/* <Route path="/user/profile" component={Profile} exact/> */}
-                    {/* <Route path="/user/Notifications" component={Notifications} exact/> */}
-                </Switch>
-            </Router>
-        );
-    }
-=======
   render() {
     return (
       <Router>
@@ -45,7 +20,7 @@ class App extends React.Component {
           {/* <Route path="/auth/login" component={Login} exact/> */}
           {/* <Route path="/auth/signup" component={Signup} exact/> */}
           {/* <Route path="/auth/forgot-password" component={ForgotPassword} exact/> */}
-          {/* <Route path="/find" component={Explore} exact/> */}
+          <Route path="/find" component={Explore} exact/>
           {/* <Route path="/find/search/result" component={SearchResult} exact/> */}
           {/* <Route path="/find/flight/details" component={FlightDetails} exact/> */}
           <Route path="/user/booking" component={MyBooking} exact />
@@ -59,6 +34,5 @@ class App extends React.Component {
       </Router>
     );
   }
->>>>>>> origin/development
 }
 export default App;
