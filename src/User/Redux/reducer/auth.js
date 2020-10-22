f5ed9754eb19b293b62e702ca0ba54c350d52c3c
prmstrgt <pramestiregitaa@gmail.com>
2020-10-22 11:12:52 +0700

    error login message

--- src/User/Redux/reducer/auth.js
+++ src/User/Redux/reducer/auth.js
@@ -18,14 +18,14 @@ export default (state = initialState, action) => {
       ...state,
       isLoading: false,
       isError: true,
-      alertMsg: action.payload.response.data.message,
+      alertMsg: action.payload.response.data.error,
     };
   }
   case "AUTH_USER_FULFILLED": {
     localStorage.setItem("token", action.payload.data.token);
     return {
       ...state,
-      token: action.payload.data.token,
+      token: action.payload.data.message.token,
       isLoading: false,
       isLogin: true,
       alertMsg: "Successfully login",
