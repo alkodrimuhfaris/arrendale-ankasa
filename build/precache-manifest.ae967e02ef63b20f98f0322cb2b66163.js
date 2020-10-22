d1fba430f1b7b0d27aa623cb467131ef19fc8c23
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-10-22 13:32:30 +0700

    build production

---
+++ build/precache-manifest.ae967e02ef63b20f98f0322cb2b66163.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef0899d24c8575ce63d49fd31212972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e274c239ba495c661aaa",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6191950955db7c3240cd",
+    "url": "/static/css/main.eb6890f5.chunk.css"
+  },
+  {
+    "revision": "e274c239ba495c661aaa",
+    "url": "/static/js/2.a72f6cdc.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/static/js/2.a72f6cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6191950955db7c3240cd",
+    "url": "/static/js/main.24df69cb.chunk.js"
+  },
+  {
+    "revision": "0180b1ec37d4a38b990a",
+    "url": "/static/js/runtime-main.91630d7b.js"
+  },
+  {
+    "revision": "ba25a9deab6bd03119a479f60b6b6d32",
+    "url": "/static/media/Circle-Checklist.ba25a9de.svg"
+  },
+  {
+    "revision": "33bb2f56db6ee4662cc1068528f44843",
+    "url": "/static/media/Circle-unChecklist.33bb2f56.svg"
+  },
+  {
+    "revision": "0f00cfea2d0711982b1da29c0592689f",
+    "url": "/static/media/bell.0f00cfea.svg"
+  },
+  {
+    "revision": "093091c260df482429cebe5e23d2ffce",
+    "url": "/static/media/blue-horizontal-switch.093091c2.svg"
+  },
+  {
+    "revision": "84e19f4397426f96597ea0fff98049a8",
+    "url": "/static/media/danger.84e19f43.svg"
+  },
+  {
+    "revision": "596cdf54100056aa980fb71eaea981e5",
+    "url": "/static/media/destination1.596cdf54.png"
+  },
+  {
+    "revision": "0e12a08eabcdb9dfc9523e6c97945a3e",
+    "url": "/static/media/destination2.0e12a08e.png"
+  },
+  {
+    "revision": "c80cabc0830099cc8e7037f9a2662e31",
+    "url": "/static/media/destination3.c80cabc0.png"
+  },
+  {
+    "revision": "d5763e701774bba4cb23f3ae26686a25",
+    "url": "/static/media/down.d5763e70.svg"
+  },
+  {
+    "revision": "159a2f89f06faecfb796741b3499283e",
+    "url": "/static/media/facebook.159a2f89.svg"
+  },
+  {
+    "revision": "f98779cacab2600e95ed3e2ba0fda929",
+    "url": "/static/media/flight.f98779ca.svg"
+  },
+  {
+    "revision": "ec057222ea6242da5165405a953c0ee5",
+    "url": "/static/media/google.ec057222.svg"
+  },
+  {
+    "revision": "e92b09b01d74e423f8ec8b41a54478ae",
+    "url": "/static/media/horizontal-switch.e92b09b0.svg"
+  },
+  {
+    "revision": "781951476d01f897f9e232cfc3dcc14b",
+    "url": "/static/media/illustration.78195147.svg"
+  },
+  {
+    "revision": "a417b6c2b3c249091dc1e0c1b8469c7a",
+    "url": "/static/media/left.a417b6c2.svg"
+  },
+  {
+    "revision": "2e7a6ae1786382963bb790ecdd1dbf32",
+    "url": "/static/media/logo-sm.2e7a6ae1.svg"
+  },
+  {
+    "revision": "300a62e8decd211fdb416c95c373d0c1",
+    "url": "/static/media/logo.300a62e8.svg"
+  },
+  {
+    "revision": "87ee53383129a5eead684c8e3a6fa483",
+    "url": "/static/media/luggage.87ee5338.svg"
+  },
+  {
+    "revision": "170aee65371b1a052ded4c880530ce10",
+    "url": "/static/media/mail.170aee65.svg"
+  },
+  {
+    "revision": "3e28b00a1221378f14fbb6599a612f68",
+    "url": "/static/media/meal.3e28b00a.svg"
+  },
+  {
+    "revision": "6ba402995f1f0e5eb35f35f20935a152",
+    "url": "/static/media/medium-flight.6ba40299.svg"
+  },
+  {
+    "revision": "4ebacbfd9a2e78cccf4813ec9f3e0c1f",
+    "url": "/static/media/paris.4ebacbfd.jpg"
+  },
+  {
+    "revision": "b71beddf16c9af81c37ed67a8ec066cb",
+    "url": "/static/media/rounde-trip.b71beddf.svg"
+  },
+  {
+    "revision": "b1a4ba102f60cc7027769c5dc2cf5937",
+    "url": "/static/media/search.b1a4ba10.svg"
+  },
+  {
+    "revision": "2d738644f482053cdda2e23e8ded7ded",
+    "url": "/static/media/sort.2d738644.svg"
+  },
+  {
+    "revision": "3143b83460af84b9bce9bbbdd6fcf275",
+    "url": "/static/media/up.3143b834.svg"
+  },
+  {
+    "revision": "97912ee3adad4bebcf3c7db8d4b98a60",
+    "url": "/static/media/vertical-switch.97912ee3.svg"
+  },
+  {
+    "revision": "274eb567341664fa0c8a98b0c9651f5b",
+    "url": "/static/media/white-flight.274eb567.svg"
+  },
+  {
+    "revision": "046fe968e6f13ff353b3c314cc5a4545",
+    "url": "/static/media/wifi.046fe968.svg"
+  }
+]);
