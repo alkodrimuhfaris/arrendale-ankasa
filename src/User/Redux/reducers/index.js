d91173c19f0be738ca7399bbfcac20d34521efd4
asrofhy <asrofiw@users.noreply.github.com>
2020-10-25 11:32:42 +0700

    implementing redux in Explore Page to get data city

--- src/User/Redux/reducers/index.js
+++ src/User/Redux/reducers/index.js
@@ -6,11 +6,13 @@ import auth from "./auth";
 import profile from "./profile";
 import flight from "./flight";
 import searchResult from "./searchResult"; 
+import city from "./city";
 
 export default combineReducers({
   searchResult,
   auth,
   user,
   flight,
-  profile
+  profile,
+  city,
 });
